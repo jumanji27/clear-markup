ecb41bc8b46a9d3b89e49dba89cd9664c4104d24
nazz-nazz <niklebdes@gmail.com>
2012-12-03 17:31:17 +0300

    new pm

---
+++ _dev/perfect_markup.js
@@ -0,0 +1,134 @@
+// perfectmarkup.js
+// Unknown script for pixel perfect html/css coding
+// https://github.com/nazz-nazz/perfect_markup
+var pm = {
+
+  initialize: {
+
+    go: function( img ) {
+
+      var overlay = pm.help.ce("div");
+      var overlayImg = pm.initialize.go.prototype.overlayImg = pm.help.ce("img");
+
+      pm.help.sa( overlay, "id", "overlay" );
+      document.body.appendChild( overlay );
+
+      pm.help.sa( overlayImg, "id", "overlay_img" );
+
+      overlay.appendChild( overlayImg );
+
+      if ( pm.doDisplay() == "" ) {
+        pm.doDisplay("none");
+      }
+
+      pm.addGlobalStyle(
+        "#overlay {" +
+        " display: " + pm.doDisplay() + ";" +
+        " position: absolute;" +
+        " top: 0;" +
+        " left: 0;" +
+        " width: 100%;" +
+        " z-index: 99;" +
+        " opacity: 0.5" +
+        "}" +
+        "#overlay img {" +
+        " display: block;" +
+        " margin: 0 auto;" +
+        "}"
+      );
+
+      var keyEvent = "keypress";
+
+      // Add hotkeys
+      pm.addEvent( document, keyEvent, function ( event ) {
+          event = event || window.event;
+          var key = event.keyCode || event.which;
+
+          // Shift + Space to show / Hide overlay
+          if ( event.shiftKey && (key == 32) ) {
+            ( pm.doDisplay() == "none" ) ? pm.doDisplay("block") : pm.doDisplay("none");
+
+            if ( event.preventDefault ) {
+              event.preventDefault();
+            }
+            event.returnValue = false;
+
+            return true;
+          }
+
+          // Ctrl + Enter - resize window to size of layout
+          if ( event.ctrlKey && (key == 13) ) {
+            $("#overlay").css( "opacity", "1" );
+          }
+      });
+
+      pm.setLayoutImage( img );
+    }
+
+  },
+
+  // Crossbrowser addEventListener
+  addEvent: function( obj, event, handler ) {
+    if ( obj.addEventListener ) {
+      obj.addEventListener( event, handler, false );
+    }
+    else if
+      ( obj.attachEvent( 'on' + event, handler ) );
+  },
+
+  addGlobalStyle: function( css ) {
+    var globalStyle = pm.help.ce("style");
+    pm.help.sa( globalStyle, "type", "text/css" );
+
+    var cssText = document.createTextNode( css );
+    globalStyle.appendChild( cssText );
+
+    document.getElementsByTagName('head')[0].appendChild( globalStyle );
+  },
+
+  setLayoutImage: function( src ) {
+    var img = new Image();
+    var overlayW, overlayH;
+    var overlayImg = pm.initialize.go.prototype.overlayImg;
+
+    pm.help.sa( overlayImg, "id", "overlay_img" );
+
+    pm.addEvent( img, "load", function () {
+      overlayImg.src = src;
+      overlayW = img.width;
+      overlayH = img.height;
+    });
+
+    img.src = src;
+  },
+
+  doDisplay: function( val ) {
+    if ( val ) {
+      overlay.style.display = val;
+      window.name = val;
+    }
+
+    return window.name;
+  },
+
+  // window.name storage routine
+  help: {
+
+    ge: function(id) {
+      return document.getElementById( id );
+    },
+
+    ce: function(tag) {
+      return document.createElement( tag );
+    },
+
+    sa: function( el, attr, val ) {
+      el.setAttribute( attr, val );
+    },
+
+  }
+
+};
+
+pm.initialize.go( "_dev/layout.png" );
+
